57c7eae0ca985db1a774f08f9ae61a083ff412ef
AlexisLedouble <a.ledouble@rubika-edu.com>
2019-10-14 11:13:51 +0200

    Init Network (Create lobby, create or join room, display players and join game)

---
+++ Assets/Network/Scripts/UI/RoomCanvasGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCanvasGroup : MonoBehaviour
+{
+   [SerializeField]
+   private CreateOrJoinRoomCanvas createOrJoinCanvas;
+   public CreateOrJoinRoomCanvas CreateOrJoinRoomCanvas { get { return createOrJoinCanvas;} }
+
+   [SerializeField]
+   private CurrentRoomCanvas currentRoomCanvas;
+   public CurrentRoomCanvas CurrentRoomCanvas { get { return currentRoomCanvas; } }
+
+    private void Awake() {
+        FirstInitialize();
+    }
+
+    private void FirstInitialize(){
+        CreateOrJoinRoomCanvas.FirstInitialize(this);
+        CurrentRoomCanvas.FirstInitialize(this);
+        
+    }
+}
