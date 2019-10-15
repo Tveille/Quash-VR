aa728e6b5ff737279794ff905e263c5b4f62570b
francoisnoel <f.noel@rubika-edu.com>
2019-10-15 19:55:52 +0200

    Ajout de TextMeshPro, Ajout d'un dossier sprite

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
