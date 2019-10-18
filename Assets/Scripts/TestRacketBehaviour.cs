f7009384167dc6ec3e3b63dfbd26e5502f096578
Rubika-TPiau <t.piau@Rubika-edu.com>
2019-10-18 23:22:37 +0200

    Ajout du script TestRacketBehaviour pour l'interraction Racket/Ball (stil in progress)

---
+++ Assets/Scripts/TestRacketBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRacketBehaviour : MonoBehaviour
+{
+    public GameObject Racket; 
+
+    private Vector3 positionTMinus2;
+    private Vector3 positionTMinus1;
+    private float lastFixedDeltaT;
+
+    private void Start()
+    {
+        positionTMinus1 = gameObject.transform.position;
+        positionTMinus2 = gameObject.transform.position;
+        lastFixedDeltaT = 1;
+    }
+
+    void FixedUpdate()
+    {
+        positionTMinus2 = positionTMinus1;
+        positionTMinus1 = gameObject.transform.position;
+        lastFixedDeltaT = Time.fixedDeltaTime;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return ((positionTMinus1 - positionTMinus2) / lastFixedDeltaT + (gameObject.transform.position) / Time.fixedDeltaTime)/2;
+    }
+}
