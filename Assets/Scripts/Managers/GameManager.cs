5c440491417af1cae4850e637bd86bd374e4e617
a.ledouble <a.ledouble@rubika-edu.com>
2019-10-19 19:23:25 +0200

    Network instantiate and synchronize players position

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject prefabPlayer;
+
+    void Awake(){
+        PhotonNetwork.Instantiate(prefabPlayer.name, prefabPlayer.transform.position, Quaternion.identity, 0);
+    }
 
     public void RestartScene()
     {
