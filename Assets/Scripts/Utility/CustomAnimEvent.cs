c8a06017aa38b6a0054c8af1734d63c7ddfb71ce
vincent <v.trinel@rubika-edu.com>
2019-10-24 09:07:01 +0200

    Houdini test

---
+++ Assets/Scripts/Utility/CustomAnimEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomAnimEvent : MonoBehaviour
+{
+    public EventAnimation[] eventAnimations;
+
+    public void AnimEvent(int eventIndex)
+    {
+        eventAnimations[eventIndex].animEvent.Invoke();
+    }
+}
+
+[System.Serializable]
+public class EventAnimation
+{
+    public string eventName = "myEvent";
+    public UnityEvent animEvent;
+
+}
---
+++ Assets/Scripts/Utility/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotSpeed = 10f;
+    public Vector3 axis = new Vector3(1, 1, 1);
+    public bool debug = false;
+
+    void Update()
+    {
+        Rotate(Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(debug)
+        Rotate(Time.deltaTime);
+    }
+
+    private void Rotate(float delta)
+    {
+        transform.Rotate(axis * delta * rotSpeed);
+    }
+}
