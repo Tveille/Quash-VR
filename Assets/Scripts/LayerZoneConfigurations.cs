e1e9b6ce8b1071ab0bef9f2c17c22b81f61e179b
francoisnoel <f.noel@rubika-edu.com>
2019-10-31 17:17:55 +0100

    Creation des zones mais pas de leur behaviour (je pull pour pouvoir récupérer la destruction du script "Level Manager")

---
+++ Assets/Scripts/LayerZoneConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerZoneConfigurations : ScriptableObject
+{
+    public ZoneSettings[] configurations;
+    public float delayBeforeNextConfiguration;
+}
+
+public struct ZoneSettings
+{
+    public int zoneID;
+    public Vector3 zonePosition;
+    public float duration;
+}
