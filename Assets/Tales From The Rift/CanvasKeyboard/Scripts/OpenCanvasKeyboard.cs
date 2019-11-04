5e36f82fd55759c34ef0e422f825a7fe08238faa
AlexisLedouble <a.ledouble@rubika-edu.com>
2019-11-04 09:49:59 +0100

    Add keyboard for menu

---
+++ Assets/Tales From The Rift/CanvasKeyboard/Scripts/OpenCanvasKeyboard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TalesFromTheRift
+{
+	public class OpenCanvasKeyboard : MonoBehaviour 
+	{
+		// Canvas to open keyboard under
+		public Canvas CanvasKeyboardObject;
+
+		// Optional: Input Object to receive text 
+		public GameObject inputObject;
+
+		public void Start(){
+			CanvasKeyboard.Open(CanvasKeyboardObject, inputObject != null ? inputObject : gameObject);
+		}
+
+		public void OpenKeyboard() 
+		{		
+			CanvasKeyboard.Open(CanvasKeyboardObject, inputObject != null ? inputObject : gameObject);
+		}
+
+		public void CloseKeyboard() 
+		{		
+			CanvasKeyboard.Close ();
+		}
+	}
+}
