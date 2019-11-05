32b5c6e86cb5f53d2e14266f8e29b0c0d9f584ae
Vince <v.trinel@rubika-edu.com>
2019-11-05 14:02:42 +0100

    Acordeon Environment HDA

---
+++ Assets/Scripts/Utility/Floatator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floatator : MonoBehaviour
+{
+    [SerializeField] float floatDuration = 1;
+    [SerializeField] Vector3 localTargetPos;
+}
