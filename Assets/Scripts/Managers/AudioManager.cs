5fd7f3781800aec91b85c757eadb92d481b14ed0
SalleVR-2 <t.veille@rubika-edu.com>
2019-11-05 14:49:24 +0100

    AudioManager WIP

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public SoundClass[] sounds;
+
+    private void Awake()
+    {
+
+        foreach (SoundClass s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            //s.source.outputAudioMixerGroup = s.output;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.spatialBlend = s.spatialBlend;
+            s.source.panStereo = s.panStereo;
+        }
+    }
+
+    public void Play(string name)
+    {
+        SoundClass s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("SOUND NOT FOUND");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        SoundClass s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("SOUND NOT FOUND");
+            return;
+        }
+
+        s.source.Stop();
+    }
+
+    public bool isPlaying(string name)
+    {
+        SoundClass s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("SOUND NOT FOUND");
+            return false;
+        }
+
+        return s.source.isPlaying;
+    }
+
+    // TO PLAY A SOUND IN ANOTHER SCRIPT :
+    //
+    // FindObjectOfType<AudioManager>().Play("name");
+    // FindObjectOfType<AudioManager>().Stop("name");
+    //
+    //"name" is equal to the string of the sound you want to play
+}
---
+++ Assets/Scripts/Managers/SoundClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class SoundClass
+{
+    public string name;
+
+    public AudioClip clip;
+    //public AudioMixerGroup output;
+
+    [Range(0.0f, 1.0f)]
+    public float volume = 1;
+    [Range(0.1f, 3.0f)]
+    public float pitch = 1.0f;
+
+    public bool loop;
+
+    [Range(0.0f, 1.0f)]
+    public float spatialBlend = 0.0f;
+
+    [Range(-1.0f, 1.0f)]
+    public float panStereo = 0.0f;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
