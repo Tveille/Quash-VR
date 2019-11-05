ec30a9d30f50c85cdb5fda423e3c2d3d59c8c3af
Vince <v.trinel@rubika-edu.com>
2019-11-05 17:07:39 +0100

    BallEvent Manager

---
+++ Assets/Scripts/Monobehaviour/BallEventManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEventManager : MonoBehaviour
+{
+    public static BallEventManager instance;
+
+    public event Action OnCollisionWithBrick = delegate { };
+    public event Action OnCollisionWithFrontWall = delegate { };
+    public event Action OnCollisionWithWall = delegate { };
+    public event Action OnCollisionWithRacket = delegate { };
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnBallCollision(string otherTag)
+    {
+        switch (otherTag)
+        {
+            case "Racket":
+                OnCollisionWithRacket();
+                break;
+            case "Wall":
+                OnCollisionWithWall();
+                break;
+            case "FrontWall":
+                OnCollisionWithFrontWall();
+                break;
+            case "Brick":
+                OnCollisionWithBrick();
+                break;
+            default:
+                break;
+        }
+    }
+}
