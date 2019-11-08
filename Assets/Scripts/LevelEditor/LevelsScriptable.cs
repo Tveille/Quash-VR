dedcd3d11e9ec7d8d190a6ec3b44b3eaff9f6589
SalleVR-2 <t.veille@rubika-edu.com>
2019-11-08 19:57:48 +0100

    Préparation pour la reprise du Level Editor

--- Assets/Scripts/LevelEditor/LevelsScriptable.cs
+++ Assets/Scripts/LevelEditor/LevelsScriptable.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Level Settings Asset", menuName = "Custom/LevelConfig", order = 120)]
 [System.Serializable]
-public class LevelSettings : ScriptableObject
+public class LevelsScriptable : ScriptableObject
 {
 
     public Sprite background;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class LevelSettings
+{
+    public WallBuilds levelWallBuilds;
+    public LayerZoneConfigurations levelZoneConfig;
 }
