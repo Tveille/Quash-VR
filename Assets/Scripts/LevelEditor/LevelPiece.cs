5cc5818d9b1059c57c132573403f825f2ee85131
SalleVR-2 <t.veille@rubika-edu.com>
2019-11-10 18:52:50 +0100

    Préparation 02

--- Assets/Scripts/LevelEditor/LevelPiece.cs
+++ Assets/Scripts/LevelEditor/LevelPiece.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class LevelPiece : MonoBehaviour
 {
     // public ParticleSystem hit;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 }
 
